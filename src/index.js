af3de59624b41aa2e540348c3471c80cd761057e
Matr0skin <>
2022-01-09 16:09:30 +0300

    Added setData function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import {validateIp} from './helpers'
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
+const ipInfo = document.getElementById('ip');
+const locationInfo = document.getElementById('location');
+const timezoneInfo = document.getElementById('timezone');
+const ispInfo = document.getElementById('isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
@@ -10,7 +15,7 @@ function getData() {
     if (validateIp(ipInput.value)) {
         fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_3F6515VRPm9BLgxaBKkIOFNUsdwAl&ipAddress=${ipInput.value}`)
             .then(response => response.json())
-            .then(console.log);
+            .then(setInfo);
     }
 }
 
@@ -18,4 +23,12 @@ function handleKey(event) {
     if (event.key === 'Enter') {
         getData();
     }
+}
+
+function setInfo(mapData) {
+    console.log(mapData);
+    ipInfo.innerText = mapData.ip;
+    locationInfo.innerText = mapData.location.country + ' ' + mapData.location.region;
+    timezoneInfo.innerText = mapData.location.timezone;
+    ispInfo.innerText = mapData.isp;
 }
