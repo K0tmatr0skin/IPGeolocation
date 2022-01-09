5b483c54e3c874cc0dcf13dfca203a1dac47c41b
Matr0skin <>
2022-01-09 15:15:28 +0300

    Added IP validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {validateIp} from './helpers'
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
@@ -5,9 +7,11 @@ btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
 function getData() {
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_3F6515VRPm9BLgxaBKkIOFNUsdwAl&ipAddress=${ipInput.value}`)
-        .then(response => response.json())
-        .then(console.log);
+    if (validateIp(ipInput.value)) {
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_3F6515VRPm9BLgxaBKkIOFNUsdwAl&ipAddress=${ipInput.value}`)
+            .then(response => response.json())
+            .then(console.log);
+    }
 }
 
 function handleKey(event) {
